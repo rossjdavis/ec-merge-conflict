fab5cb0b636df0beeb84bb72709872d651bf2aec
Ross Davis <rossjdavis@gmail.com>
2017-12-14 15:32:35 -0500

    step 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
+import "./Hello"
+import "./Compliment"
 
 class App extends Component {
   render() {
@@ -14,6 +16,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Hello name={"Ross"} />
+        <Compliment />
       </div>
     )
   }
