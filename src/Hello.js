15c08e9ace0bd82c28596f2e6edc07b632c3153e
Ross Davis <rossjdavis@gmail.com>
2017-12-14 15:09:40 -0500

    hello ross

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component }n from "react"
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>Hello {this.props.name}</div>
+    )
+  }
+}
+
+export default Hello
